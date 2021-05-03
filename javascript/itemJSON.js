06199ee88c457ad8c51a154326c165008fb9f79a
Blacklist14 <luis.arigato69@hotmail.com>
2021-05-02 22:54:34 -0300

    update 0.4

--- javascript/itemJSON.js
+++ javascript/itemJSON.js
@@ -1,8 +1,3 @@
-//1: O JSON DOS ITENS
-//2:FILTRA OS ITENS DO JSON POR VALOR
-//3: FILTRA OS ITENS DO JSON POR COR
-
-
 /* Json-itens-lojas */
 let itemJSON = [{
     "imagem": "https://i.pinimg.com/564x/74/f9/93/74f993732316a6e6569b3da688beb318.jpg",
@@ -18,7 +13,7 @@ let itemJSON = [{
     "valor": "168.50",
     "valorString":"168.50",
     "cor":"rosa",
-    "tamanho":36
+    "tamanho":"36"
 },
 {
     "imagem": "https://i.pinimg.com/originals/81/f1/38/81f138d71a8778bded487b0c56cf4e9f.png",
@@ -67,73 +62,105 @@ let itemJSON = [{
     "valorString":"275,00",
     "cor":"laranja",
     "tamanho":"40"
+},
+{
+    "imagem": "https://i.pinimg.com/236x/be/91/90/be9190624b61a130eaf982749257e210.jpg",
+    "nome": "cardigan tigre",
+    "valor": "275.00",
+    "valorString":"275,00",
+    "cor":"laranja",
+    "tamanho":"40"
+},
+{
+    "imagem": "https://i.pinimg.com/236x/9e/24/1e/9e241e9c038777f7d00329bdd5e06bfa.jpg",
+    "nome": "saia em couro",
+    "valor": "43.00",
+    "valorString":"43,00",
+    "cor":"marrom",
+    "tamanho":"g"
+},
+{
+    "imagem": "https://i.pinimg.com/564x/74/f9/93/74f993732316a6e6569b3da688beb318.jpg",
+    "nome": "cropped stripes",
+    "valor": "120.00",
+    "valorString":"120,00",
+    "cor":"verde",
+    "tamanho":"p"
 }
 ]
 
 /*COLOCA OS JSON NA TELA*/
-let itemImg = document.querySelectorAll(".item-img")
-    itemImg.forEach((props, val) => {
-        props.setAttribute("src", `${itemJSON[val].imagem}`)
+const render = ()=>{
+    let itemContent = document.querySelectorAll(".item-content")
+    let itemImg = document.querySelectorAll(".item-img")
+itemImg.forEach((props, val) => {
+    props.setAttribute("src", `${itemJSON[val].imagem}`)
 })
 
 let itemName = document.querySelectorAll(".item-name")
-    itemName.forEach((props, val) => {
-        props.innerHTML = itemJSON[val].nome.toUpperCase()
+itemName.forEach((props, val) => {
+    props.innerHTML = itemJSON[val].nome.toUpperCase()
 })
 
 let itemValue = document.querySelectorAll(".item-value")
-    itemValue.forEach((props, val) => {
-        props.innerHTML = "R$ " + itemJSON[val].valorString
+itemValue.forEach((props, val) => {
+    props.innerHTML = "R$ " + itemJSON[val].valorString
 })
-
+}
+render()
 /* FILTRAÇÃO POR VALOR */
 let filter1 = 0
 let filter2 = 0
 let filter3 = 0
 let filter4 = 0
 let filter5 = 0
-
 let itemContent = document.querySelectorAll(".item-content")
-let displayItem = ()=> itemContent.forEach((props,val)=>{
-    props.style.display = "flex"
-})
-let contentFilter = ()=>{itemContent.forEach((props,val)=>{
-    props.style.display = "none"
-})}
 
-document.querySelector("#faixa-50").addEventListener("click",()=>{
-    contentFilter() 
-    if(filter1 <= 0){
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].valor > 0){
-            if(itemJSON[val].valor < 50){
-                contentFilter
-                itemContent[val].style.display = "flex"
-            }
-        }
+let displayItem = () =>{
+        itemContent.forEach((props, val) => {
+            itemContent[val].style.display = "flex"
+            console.log(itemContent[val])
+        })
+}
+let contentFilter = () => {
+    itemContent.forEach((props, val) => {
+        props.style.display = "none"
     })
+}
+
+document.querySelector("#faixa-50").addEventListener("click", () => {
+    contentFilter()
+    if (filter1 <= 0) {
+        itemJSON.forEach((props, val) => {
+            if (itemJSON[val].valor > 0) {
+                if (itemJSON[val].valor < 50) {
+                    contentFilter
+                    itemContent[val].style.display = "flex"
+                }
+            }
+        })
     }
     filter1++
-    if(filter1 >=2){
+    if (filter1 >= 2) {
         displayItem()
         filter1 = 0
     }
 
 })
 
-document.querySelector("#faixa-150").addEventListener("click",()=>{
-    contentFilter() 
-    if(filter2 <= 0){
-        itemJSON.forEach((props,val)=>{
-            if(itemJSON[val].valor > 51){
-                if(itemJSON[val].valor < 150){
+document.querySelector("#faixa-150").addEventListener("click", () => {
+    contentFilter()
+    if (filter2 <= 0) {
+        itemJSON.forEach((props, val) => {
+            if (itemJSON[val].valor > 51) {
+                if (itemJSON[val].valor < 150) {
                     itemContent[val].style.display = "flex"
                 }
             }
         })
     }
     filter2++
-    if(filter2 >= 2){
+    if (filter2 >= 2) {
         displayItem()
         filter2 = 0
     }
@@ -141,11 +168,11 @@ document.querySelector("#faixa-150").addEventListener("click",()=>{
 
 document.querySelector("#faixa-300").addEventListener("click",()=>{
     contentFilter()
+    let itemContent = document.querySelectorAll(".item-content")
     if(filter3 <= 0){   
     itemJSON.forEach((props,val)=>{
         if(itemJSON[val].valor > 151){
             if(itemJSON[val].valor < 300){
-                contentFilter
                 itemContent[val].style.display = "flex"
             }
         }
@@ -157,13 +184,12 @@ document.querySelector("#faixa-300").addEventListener("click",()=>{
         filter3 = 0
     }
 })
-
-document.querySelector("#faixa-500").addEventListener("click",()=>{
+document.querySelector("#faixa-500").addEventListener("click", () => {
     contentFilter()
-    if(filter4 <= 0){
-        itemJSON.forEach((props,val)=>{
-            if(itemJSON[val].valor > 301){
-                if(itemJSON[val].valor < 500){
+    if (filter4 <= 0) {
+        itemJSON.forEach((props, val) => {
+            if (itemJSON[val].valor > 301) {
+                if (itemJSON[val].valor < 500) {
                     contentFilter()
                     itemContent[val].style.display = "flex"
                 }
@@ -171,189 +197,218 @@ document.querySelector("#faixa-500").addEventListener("click",()=>{
         })
     }
     filter4++
-    if(filter4 >= 2){
+    if (filter4 >= 2) {
         displayItem()
         filter4 = 0
     }
 })
 
-document.querySelector("#faixa-aparti-1").addEventListener("click",()=>{
+document.querySelector("#faixa-aparti-1").addEventListener("click", () => {
     contentFilter()
-    if(filter5 <= 0){
-        itemJSON.forEach((props,val)=>{
-            if(itemJSON[val].valor > 1){
+    if (filter5 <= 0) {
+        itemJSON.forEach((props, val) => {
+            if (itemJSON[val].valor > 1) {
                 itemContent[val].style.display = "flex"
             }
         })
     }
     filter5++
-    if(filter5 >= 2){
+    if (filter5 >= 2) {
         displayItem()
         filter5 = 0
     }
 })
 
 /* FILTRA POR COR*/
-document.querySelector("#amarelo").addEventListener("click",()=>{
+document.querySelector("#amarelo").addEventListener("click", () => {
     contentFilter()
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].cor == "amarelo"){
-            itemContent[val].style.display= "flex"
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].cor == "amarelo") {
+            itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#branco").addEventListener("click",()=>{
+document.querySelector("#branco").addEventListener("click", () => {
     contentFilter()
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].cor == "branco"){
-            itemContent[val].style.display= "flex"
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].cor == "branco") {
+            itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#verde").addEventListener("click",()=>{
+document.querySelector("#verde").addEventListener("click", () => {
     contentFilter()
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].cor == "verde"){
-            itemContent[val].style.display= "flex"
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].cor == "verde") {
+            itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#rosa").addEventListener("click",()=>{
+document.querySelector("#rosa").addEventListener("click", () => {
     contentFilter()
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].cor == "rosa"){
-            itemContent[val].style.display= "flex"
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].cor == "rosa") {
+            itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#laranja").addEventListener("click",()=>{
+document.querySelector("#laranja").addEventListener("click", () => {
     contentFilter()
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].cor == "laranja"){
-            itemContent[val].style.display= "flex"
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].cor == "laranja") {
+            itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#roxo").addEventListener("click",()=>{
+document.querySelector("#roxo").addEventListener("click", () => {
     contentFilter()
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].cor == "roxo"){
-            itemContent[val].style.display= "flex"
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].cor == "roxo") {
+            itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#cinza").addEventListener("click",()=>{
+document.querySelector("#cinza").addEventListener("click", () => {
     contentFilter()
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].cor === "cinza"){
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].cor === "cinza") {
             contentFilter()
-            itemContent[val].style.display= "flex"
+            itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#azul").addEventListener("click",()=>{
+document.querySelector("#azul").addEventListener("click", () => {
     contentFilter()
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].cor == "azul"){
-            itemContent[val].style.display= "flex"
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].cor == "azul") {
+            itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#marrom").addEventListener("click",()=>{
+document.querySelector("#marrom").addEventListener("click", () => {
     contentFilter()
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].cor == "marrom"){
-            itemContent[val].style.display= "flex"
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].cor == "marrom") {
+            itemContent[val].style.display = "flex"
         }
     })
 })
 
 /*FILTRA POR TAMANHO */
-document.querySelector("#tamanho-p").addEventListener("click",()=>{
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].tamanho == "p"){
+document.querySelector("#tamanho-p").addEventListener("click", () => {
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].tamanho == "p") {
             contentFilter()
             itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#tamanho-m").addEventListener("click",()=>{
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].tamanho == "m"){
+document.querySelector("#tamanho-m").addEventListener("click", () => {
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].tamanho == "m") {
             contentFilter()
             itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#tamanho-g").addEventListener("click",()=>{
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].tamanho == "g"){
+document.querySelector("#tamanho-g").addEventListener("click", () => {
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].tamanho == "g") {
             contentFilter()
             itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#tamanho-gg").addEventListener("click",()=>{
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].tamanho == "gg"){
+document.querySelector("#tamanho-gg").addEventListener("click", () => {
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].tamanho == "gg") {
             contentFilter()
             itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#tamanho-u").addEventListener("click",()=>{
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].tamanho == "u"){
+document.querySelector("#tamanho-u").addEventListener("click", () => {
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].tamanho == "u") {
             contentFilter()
             itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#tamanho-36").addEventListener("click",()=>{
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].tamanho == "36"){
+document.querySelector("#tamanho-36").addEventListener("click", () => {
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].tamanho == "36") {
             contentFilter()
             itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#tamanho-38").addEventListener("click",()=>{
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].tamanho == "38"){
+document.querySelector("#tamanho-38").addEventListener("click", () => {
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].tamanho == "38") {
             contentFilter()
             itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#tamanho-40").addEventListener("click",()=>{
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].tamanho == "40"){
+document.querySelector("#tamanho-40").addEventListener("click", () => {
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].tamanho == "40") {
             contentFilter()
             itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#tamanho-42").addEventListener("click",()=>{
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].tamanho == "42"){
+document.querySelector("#tamanho-42").addEventListener("click", () => {
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].tamanho == "42") {
             contentFilter()
             itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#tamanho-44").addEventListener("click",()=>{
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].tamanho == "44"){
+document.querySelector("#tamanho-44").addEventListener("click", () => {
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].tamanho == "44") {
             contentFilter()
             itemContent[val].style.display = "flex"
         }
     })
 })
-document.querySelector("#tamanho-46").addEventListener("click",()=>{
-    itemJSON.forEach((props,val)=>{
-        if(itemJSON[val].tamanho == "46"){
+document.querySelector("#tamanho-46").addEventListener("click", () => {
+    itemJSON.forEach((props, val) => {
+        if (itemJSON[val].tamanho == "46") {
             contentFilter()
             itemContent[val].style.display = "flex"
         }
+    })
+})
+
+/*MOSTRA MAIS PRODUTOS */
+
+let refil = 0
+document.querySelector("#item-carregar").addEventListener("click", () => {
+    if(refil < 3){
+        let caixa = document.querySelector("#produtos-main")
+        let caixinha = `<div class="item-content">
+        <img class="item-img" alt="">
+        <p class="item-name">teste</p>
+        <span class="item-value"></span>
+        <p class="item-div">até 3x de R$40,00</p>
+        <button class="item-button">COMPRAR</button>
+        </div>`
+        caixa.innerHTML= caixa.innerHTML + caixinha
+    }
+    refil++
+    render()
+})
+
+/*COMPRA ITENS */
+let number = 1
+buttonCompra = document.querySelectorAll(".item-button")
+buttonCompra.forEach((props,val)=>{
+    props.addEventListener("click",()=>{
+        let carrinho = document.querySelector("#card-not")
+        carrinho.innerHTML = number++
     })
 })
